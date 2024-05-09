248818aea1cc8b7f25a6aea9dd8886c49c175856
rlaehrud09 <dk8352.kim@gmail.com>
2024-05-09 16:00:54 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -4,7 +4,14 @@ input.onButtonPressed(Button.B, function () {
     제한 += 1
     basic.pause(100)
     music.stopAllSounds()
-    if (60 == 제한) {
+    if (완료 == 25) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+        basic.showString("YOU WIN")
+        basic.pause(2000)
+        music.stopAllSounds()
+        basic.clearScreen()
+    }
+    if (80 == 제한) {
         music.stopAllSounds()
         basic.clearScreen()
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Baddy), music.PlaybackMode.InBackground)
@@ -21,5 +28,85 @@ input.onButtonPressed(Button.B, function () {
         music.stopAllSounds()
     }
 })
+let 완료 = 0
 let 제한 = 0
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
+basic.forever(function () {
+    if (led.point(0, 0)) {
+        완료 += 1
+    }
+    if (led.point(0, 1)) {
+        완료 += 1
+    }
+    if (led.point(0, 2)) {
+        완료 += 1
+    }
+    if (led.point(0, 3)) {
+        완료 += 1
+    }
+    if (led.point(0, 4)) {
+        완료 += 1
+    }
+    if (led.point(1, 0)) {
+        완료 += 1
+    }
+    if (led.point(1, 1)) {
+        완료 += 1
+    }
+    if (led.point(1, 2)) {
+        완료 += 1
+    }
+    if (led.point(1, 3)) {
+        완료 += 1
+    }
+    if (led.point(1, 4)) {
+        완료 += 1
+    }
+    if (led.point(2, 0)) {
+        완료 += 1
+    }
+    if (led.point(2, 1)) {
+        완료 += 1
+    }
+    if (led.point(2, 2)) {
+        완료 += 1
+    }
+    if (led.point(2, 3)) {
+        완료 += 1
+    }
+    if (led.point(2, 4)) {
+        완료 += 1
+    }
+    if (led.point(3, 0)) {
+        완료 += 1
+    }
+    if (led.point(3, 1)) {
+        완료 += 1
+    }
+    if (led.point(3, 2)) {
+        완료 += 1
+    }
+    if (led.point(3, 3)) {
+        완료 += 1
+    }
+    if (led.point(3, 4)) {
+        완료 += 1
+    }
+    if (led.point(4, 0)) {
+        완료 += 1
+    }
+    if (led.point(4, 1)) {
+        완료 += 1
+    }
+    if (led.point(4, 2)) {
+        완료 += 1
+    }
+    if (led.point(4, 3)) {
+        완료 += 1
+    }
+    if (led.point(4, 4)) {
+        완료 += 1
+    }
+    basic.pause(200)
+    완료 = 0
+})
